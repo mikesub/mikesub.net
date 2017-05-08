dfaea0d0c8f54d67e772d870dba8e4b3301e5ecb
Mike S <aa3f687f47620f6b36ed0f8ca4bfd70b223df69a>
2017-05-08 16:16:58 +0300

    migrate to mustache

--- src/render.js
+++ src/render.js
@@ -1,24 +1,26 @@
-import path from 'path';
+const fs = require('fs');
+const path = require('path');
+const _ = require('lodash');
+const mustache = require('mustache');
+const _articles = require('./articles.js');
 
-import nunjucks from 'nunjucks';
-import _ from 'lodash';
-
-import loadArticles from './articles.js';
-
-nunjucks.configure(path.resolve(__dirname, '../templates'));
-
+const templatesDir = 'templates';
 const defaultContext = {
-  siteTitle: 'Михаил Сабуренков',
+  siteTitle: 'Mike Saburenkov',
   siteURL: 'http://mikesub.net/blog',
-  siteHeading: 'Михаил Сабуренков',
+  siteHeading: 'Mike Saburenkov',
 };
 
-const articles = _.sortBy(loadArticles(), 'sortKey').reverse();
-const renderList = (template, items) => nunjucks.render(template, Object.assign({items}, defaultContext));
-const renderArticle = (template, item) => nunjucks.render(template, Object.assign({item}, defaultContext));
+const loadTemplate = fileName => fs.readFileSync(path.join(templatesDir, `${fileName}.mustache`), {encoding: 'utf8'});
+const articles = _articles.load();
+const sortedArticles = _.sortBy(articles, 'sortKey').reverse();
+const renderList = (template, items) => mustache.render(loadTemplate(template), Object.assign({items}, defaultContext));
+const renderArticle = (template, item) => mustache.render(loadTemplate(template), Object.assign({item}, defaultContext));
 
-export default {
-  index: renderList('index.html', articles),
-  rss: renderList('rss.xml', articles),
-  articles: [for (article of articles) {path: article.path, body: renderArticle('article.html', article)}],
+module.exports = {
+  index: renderList('index', sortedArticles),
+  rss: renderList('rss', sortedArticles),
+  articles: articles.map(article => ({path: article.path, body: renderArticle('article', article)})),
 };
+
+
