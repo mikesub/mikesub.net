400b30db47a5f53b3172e6e4895fd6aaa1c46925
Mike Saburenkov <allo.superman@gmail.com>
2018-09-25 21:57:27 +0200

    Support posts w/o titles

--- src/feed.js
+++ src/feed.js
@@ -1,12 +1,18 @@
 const url = 'http://mikesub.net/blog/';
 
-const item = ({title, path, body, feedDate}) => ({
-  title: title,
-  id: url + path,
-  url: url + path,
-  content_html: body,
-  date_published: feedDate,
-});
+const item = ({title, path, body, feedDate}) => {
+  let result = {
+    title: title,
+    id: url + path,
+    url: url + path,
+    content_html: body,
+    date_published: feedDate,
+  };
+  if (!title) {
+    delete result.title;
+  }
+  return result;
+};
 
 const genFeed = items => JSON.stringify({
   version: 'https://jsonfeed.org/version/1',
