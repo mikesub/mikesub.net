35ae6a1080c3c6a90517a9a1b57471ab5ec6fc1d
Mike S <aa3f687f47620f6b36ed0f8ca4bfd70b223df69a>
2019-02-25 22:54:53 +0100

    prettier

--- src/extractDescription.js
+++ src/extractDescription.js
@@ -1,12 +1,12 @@
-const ending = '...';
+const ending = "...";
 const length = 200;
 
-module.exports = function (html) {
+module.exports = function(html) {
   const words = html
-    .replace(/<\/?[^>]+>/g, '')
-    .replace(/\n/g, ' ')
-    .split(' ');
-  let result = '';
+    .replace(/<\/?[^>]+>/g, "")
+    .replace(/\n/g, " ")
+    .split(" ");
+  let result = "";
 
   for (const word of words) {
     if (result.length + word.length + ending.length > length) {
--- src/render.js
+++ src/render.js
@@ -1,22 +1,33 @@
-const fs = require('fs');
-const path = require('path');
-const _ = require('lodash');
-const mustache = require('mustache');
-const _articles = require('./articles');
-const feed = require('./feed');
+const fs = require("fs");
+const path = require("path");
+const _ = require("lodash");
+const mustache = require("mustache");
+const _articles = require("./articles");
+const feed = require("./feed");
 
-const templatesDir = 'templates';
+const templatesDir = "templates";
 
-const loadTemplate = fileName => fs.readFileSync(path.join(templatesDir, `${fileName}.mustache`), {encoding: 'utf8'});
+const loadTemplate = fileName =>
+  fs.readFileSync(path.join(templatesDir, `${fileName}.mustache`), {
+    encoding: "utf8"
+  });
 const articles = _articles.load();
-const sortedArticles = _.sortBy(articles, 'sortKey').reverse();
-const partials = {'_darkmode': loadTemplate('_darkmode'), '_analytics': loadTemplate('_analytics')};
-const renderList = (template, items) => mustache.render(loadTemplate(template), {items}, partials);
-const renderArticle = (template, item) => mustache.render(loadTemplate(template), {item}, partials);
+const sortedArticles = _.sortBy(articles, "sortKey").reverse();
+const partials = {
+  _darkmode: loadTemplate("_darkmode"),
+  _analytics: loadTemplate("_analytics")
+};
+const renderList = (template, items) =>
+  mustache.render(loadTemplate(template), { items }, partials);
+const renderArticle = (template, item) =>
+  mustache.render(loadTemplate(template), { item }, partials);
 
 module.exports = {
-  index: renderList('index', sortedArticles),
-  rss: renderList('rss', sortedArticles),
+  index: renderList("index", sortedArticles),
+  rss: renderList("rss", sortedArticles),
   feed: feed.genFeed(sortedArticles),
-  articles: articles.map(article => ({path: article.path, body: renderArticle('article', article)})),
+  articles: articles.map(article => ({
+    path: article.path,
+    body: renderArticle("article", article)
+  }))
 };
