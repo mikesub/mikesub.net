b7a8c9773a2365c42a0a4248a59081d165ab935c
Mike Saburenkov <allo.superman@gmail.com>
2019-02-26 22:10:35 +0100

    eslint

--- src/extractDescription.js
+++ src/extractDescription.js
@@ -1,19 +1,25 @@
 const ending = "...";
 const length = 200;
 
-module.exports = function(html) {
-  const words = html
-    .replace(/<\/?[^>]+>/g, "")
-    .replace(/\n/g, " ")
-    .split(" ");
-  let result = "";
+function stripTags(html) {
+  return html.replace(/<\/?[^>]+>/g, "");
+}
 
+function splitByWords(text) {
+  return text.replace(/\n/g, " ").split(" ");
+}
+
+module.exports = function(html) {
+  const words = splitByWords(stripTags(html));
+  let result = ""; // eslint-disable-line fp/no-let
+
+  // eslint-disable-next-line fp/no-loops, fp/no-nil, fp/no-mutation
   for (const word of words) {
     if (result.length + word.length + ending.length > length) {
-      result += ending;
+      result += ending; // eslint-disable-line fp/no-mutation
       break;
     }
-    result += ` ${word}`;
+    result += ` ${word}`; // eslint-disable-line fp/no-mutation
   }
 
   return result;
