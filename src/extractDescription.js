cc1c0a22481da6ece40362734dc4415efcbbef48
Mikhail Saburenkov <m.saburenkov@catawiki.nl>
2019-03-01 15:07:34 +0100

    Use truncate from lodash

--- src/extractDescription.js
+++ src/extractDescription.js
@@ -1,26 +1,16 @@
-const ending = "...";
-const length = 200;
+const _ = require("lodash");
 
 function stripTags(html) {
   return html.replace(/<\/?[^>]+>/g, "");
 }
-
-function splitByWords(text) {
-  return text.replace(/\n/g, " ").split(" ");
+function stripNewLines(text) {
+  return text.replace(/\n/g, "");
 }
 
 module.exports = function(html) {
-  const words = splitByWords(stripTags(html));
-  let result = ""; // eslint-disable-line fp/no-let
-
-  // eslint-disable-next-line fp/no-loops, fp/no-nil, fp/no-mutation
-  for (const word of words) {
-    if (result.length + word.length + ending.length > length) {
-      result += ending; // eslint-disable-line fp/no-mutation
-      break;
-    }
-    result += ` ${word}`; // eslint-disable-line fp/no-mutation
-  }
-
-  return result;
+  return _.truncate(stripNewLines(stripTags(html)), {
+    length: 200,
+    omission: "...",
+    separator: " "
+  });
 };
