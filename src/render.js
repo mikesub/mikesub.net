aa4c2ffddab5610c81dd8c814923589788afef40
Mikhail Saburenkov <m.saburenkov@catawiki.nl>
2019-03-01 15:54:16 +0100

    Change function declaration style

--- src/render.js
+++ src/render.js
@@ -2,33 +2,39 @@ const fs = require("fs");
 const path = require("path");
 const _ = require("lodash");
 const mustache = require("mustache");
-const _articles = require("./articles");
+const articles = require("./articles");
 const feed = require("./feed");
 const config = require("../config.json");
 const templatesDir = "templates";
 
-const loadTemplate = fileName =>
-  fs.readFileSync(path.join(templatesDir, `${fileName}.mustache`), {
+function loadTemplate(fileName) {
+  return fs.readFileSync(path.join(templatesDir, `${fileName}.mustache`), {
     encoding: "utf8"
   });
-const articles = _articles.load();
-// eslint-disable-next-line fp/no-mutating-methods
-const sortedArticles = _.sortBy(articles, "sortKey").reverse();
+}
+
 const partials = {
   _darkmode: loadTemplate("_darkmode"),
   _analytics: loadTemplate("_analytics")
 };
-const renderList = (template, items) =>
-  mustache.render(loadTemplate(template), { config, items }, partials);
-const renderArticle = (template, item) =>
-  mustache.render(loadTemplate(template), { config, item }, partials);
+
+function render(template, context) {
+  return mustache.render(
+    loadTemplate(template),
+    { config, ...context },
+    partials
+  );
+}
+
+// eslint-disable-next-line fp/no-mutating-methods
+const sortedArticles = _.sortBy(articles.load(), "sortKey").reverse();
 
 module.exports = {
-  index: renderList("index", sortedArticles),
-  rss: renderList("rss", sortedArticles),
+  index: render("index", { items: sortedArticles }),
+  rss: render("rss", { items: sortedArticles }),
   feed: feed.genFeed(sortedArticles),
-  articles: articles.map(article => ({
+  articles: sortedArticles.map(article => ({
     path: article.path,
-    body: renderArticle("article", article)
+    body: render("article", { item: article })
   }))
 };
