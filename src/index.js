7e775df8706653477ef0bb2e9cf16b63274d7d04
Mike S <aa3f687f47620f6b36ed0f8ca4bfd70b223df69a>
2019-03-01 15:56:03 +0100

    Move output to docs/blog

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 const fs = require("fs");
 const render = require("./render");
+const config = require("../config.json");
 
 /* eslint-disable-next-line fp/no-unused-expression */
-fs.writeFileSync("./rss.xml", render.rss);
+fs.writeFileSync(`${config.targetDir}rss.xml`, render.rss);
 /* eslint-disable-next-line fp/no-unused-expression */
-fs.writeFileSync("./index.html", render.index);
+fs.writeFileSync(`${config.targetDir}index.html`, render.index);
 /* eslint-disable-next-line fp/no-unused-expression */
-fs.writeFileSync("./feed.json", render.feed);
+fs.writeFileSync(`${config.targetDir}feed.json`, render.feed);
 /* eslint-disable-next-line fp/no-unused-expression */
 render.articles.forEach(article =>
-  fs.writeFileSync(article.path, article.body)
+  fs.writeFileSync(config.targetDir + article.path, article.body)
 );
