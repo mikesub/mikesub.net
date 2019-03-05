05a649b18597317a14b8445121bb5e2eeff774a7
Mike S <aa3f687f47620f6b36ed0f8ca4bfd70b223df69a>
2019-03-05 15:06:21 +0100

    move to TS

---
+++ src/render.ts
@@ -0,0 +1,51 @@
+import fs from "fs";
+import _ from "lodash";
+import mustache from "mustache";
+import path from "path";
+import config from "../config.json";
+import * as _articles from "./articles";
+import * as _feed from "./feed";
+
+import { Article } from "./articles";
+
+type TemplateSingleContext = {
+  item: Article;
+};
+
+type TemplateListContext = {
+  items: Article[];
+};
+
+const templatesDir = "templates";
+
+function loadTemplate(fileName: string) {
+  return fs.readFileSync(path.join(templatesDir, `${fileName}.mustache`), {
+    encoding: "utf8"
+  });
+}
+
+const partials = {
+  _darkmode: loadTemplate("_darkmode"),
+  _analytics: loadTemplate("_analytics")
+};
+
+function render(
+  template: string,
+  context: TemplateSingleContext | TemplateListContext
+) {
+  return mustache.render(
+    loadTemplate(template),
+    { config, ...context },
+    partials
+  );
+}
+
+const sortedArticles = _.sortBy(_articles.load(), "sortKey").reverse();
+
+export const index = render("index", { items: sortedArticles });
+export const rss = render("rss", { items: sortedArticles });
+export const feed = _feed.genFeed(sortedArticles);
+export const articles = sortedArticles.map(article => ({
+  path: article.path,
+  body: render("article", { item: article })
+}));
