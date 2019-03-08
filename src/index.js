b26713e381743f35b73490d7cad602959d57ed58
Mikhail Saburenkov <m.saburenkov@catawiki.nl>
2019-03-08 13:43:05 +0100

    minor refactorings

--- src/index.js
+++ src/index.js
@@ -2,13 +2,11 @@ const fs = require("fs");
 const render = require("./render");
 const config = require("../config.json");
 
-/* eslint-disable-next-line fp/no-unused-expression */
+/* eslint-disable fp/no-unused-expression */
 fs.writeFileSync(`${config.targetDir}rss.xml`, render.rss);
-/* eslint-disable-next-line fp/no-unused-expression */
 fs.writeFileSync(`${config.targetDir}index.html`, render.index);
-/* eslint-disable-next-line fp/no-unused-expression */
 fs.writeFileSync(`${config.targetDir}feed.json`, render.feed);
-/* eslint-disable-next-line fp/no-unused-expression */
 render.articles.forEach(article =>
   fs.writeFileSync(config.targetDir + article.path, article.body)
 );
+/* eslint-enable fp/no-unused-expression */
